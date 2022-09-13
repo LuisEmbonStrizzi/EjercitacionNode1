e41e37c6f555cc9ff10c7fa6db29d9944b0dd864
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-09-13 08:39:10 -0300

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Luis")
+
+//Clase nacho: Comandos para crear repo (siempre vacío)
+//git init (empezar repo)
+//git status
+//git branch -M main(cambiar de master a main)
+//git remote add origin (ruta repo)
+//git add .
+//git commit -m "feat: first commit"
+//git push origin main
