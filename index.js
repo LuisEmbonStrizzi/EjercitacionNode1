a4802380ed7623118ef3ea0ad729148c0ca400a3
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-09-20 09:30:46 -0300

    feat: Ejercicio 1 al 6 :)

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ const users = [
     { id: 4, name: "Jane Me" }
 ]
 
+const menu = require('./menu.json');
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("API running OK...")
 });
@@ -32,11 +36,64 @@ app.get("/users/:id", (req, res) => {
 
     const user = users.find((user) => user.id === id)
 
-    if(!user)
+    if (!user)
         res.status(404).send("User not found")
     else
         res.status(200).json(user)
 });
+
+//Ejercicio 1
+app.get('/menu', (req, res) => {
+    res.json(menu)
+})
+
+//Ejercicio 2
+app.get('/menu/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const plato = menu.find((plato => plato.id === id))
+
+    if (!plato)
+        res.status(404).send("Dish not found")
+    else
+        res.status(200).json(plato)
+
+})
+
+//Ejercicio 3
+
+app.get('/principales', (req, res) => {
+    const plato_principal = menu.filter((menu) => menu.tipo === 'principal')
+
+    res.json(plato_principal)
+})
+
+//Ejercicio 4
+app.get('/postres', (req, res) => {
+    const postres = menu.filter((menu) => menu.tipo === 'postre')
+
+    res.json(postres)
+})
+
+//Ejercicio 5
+app.get('/bebidas', (req, res) => {
+    const bebidas = menu.filter((menu) => menu.tipo === 'bebida')
+
+    res.json(bebidas)
+})
+
+//Ejercicio 6
+
+app.post('/pedido', (req, res) => {
+    const pedido = req.body.productos
+
+    const precio = pedido.reduce((precio_total, plato) => {
+        const precioPlato = menu.find((plato_menu) => plato_menu.id === plato.id)
+        return precio_total + precioPlato.precio * plato.cantidad
+    }, 0)
+
+    res.json({"msj": "Pedido recibido", "precio": precio})
+})
 
 app.listen(port, () => {
     console.log(`> Server running on port ${port}`)
